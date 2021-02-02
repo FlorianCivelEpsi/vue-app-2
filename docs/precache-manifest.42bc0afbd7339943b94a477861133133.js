95a4645f1e782791cf883d7be4a2ec459511e5e5
FlorianCivelEpsi <florian.civel@epsi.fr>
2021-02-02 15:22:16 +0100

    docs

---
+++ docs/precache-manifest.42bc0afbd7339943b94a477861133133.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2630375bf11aa4f1e0",
+    "url": "/vue-app-2/css/app.ddeedaa8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-app-2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "070162d0c809272fd83e8f06ffe23351",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "1a96fce5b1b624bf999d",
+    "url": "/vue-app-2/js/about.0d0ec44c.js"
+  },
+  {
+    "revision": "6f2630375bf11aa4f1e0",
+    "url": "/vue-app-2/js/app.a8771247.js"
+  },
+  {
+    "revision": "ba2a6fda9ddaaddeab4b",
+    "url": "/vue-app-2/js/chunk-vendors.f449932c.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
